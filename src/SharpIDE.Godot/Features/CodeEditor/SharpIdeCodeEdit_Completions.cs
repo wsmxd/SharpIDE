eafed42383ce12d6dd838125cdb24044e3081ac9
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-10-27 20:43:45 +1000

    local var icon v2

--- src/SharpIDE.Godot/Features/CodeEditor/SharpIdeCodeEdit_Completions.cs
+++ src/SharpIDE.Godot/Features/CodeEditor/SharpIdeCodeEdit_Completions.cs
@@ -8,6 +8,7 @@ public partial class SharpIdeCodeEdit
     private readonly Texture2D _csharpMethodIcon = ResourceLoader.Load<Texture2D>("uid://b17p18ijhvsep");
     private readonly Texture2D _csharpClassIcon = ResourceLoader.Load<Texture2D>("uid://b027uufaewitj");
     private readonly Texture2D _csharpInterfaceIcon = ResourceLoader.Load<Texture2D>("uid://bdwmkdweqvowt");
+    private readonly Texture2D _localVariableIcon = ResourceLoader.Load<Texture2D>("uid://vwvkxlnvqqk3");
 
     private Texture2D? GetIconForCompletion(SymbolKind? symbolKind, TypeKind? typeKind, Accessibility? accessibility)
     {
@@ -16,7 +17,7 @@ public partial class SharpIdeCodeEdit
             (SymbolKind.Method, _, _) => _csharpMethodIcon,
             (_, TypeKind.Interface, _) => _csharpInterfaceIcon,
             (SymbolKind.NamedType, _, _) => _csharpClassIcon,
-            //SymbolKind.Local => ,
+            (SymbolKind.Local, _, _) => _localVariableIcon,
             //SymbolKind.Property => ,
             //SymbolKind.Field => ,
             _ => null
